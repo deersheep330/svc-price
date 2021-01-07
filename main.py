a1db0573045eb9ebbf5c9a2201a8d57663e74f83
YangChiu <yangchiu@inquartik.com>
2021-01-07 16:51:02 +0800

    parsing us stock price date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pprint import pprint
+from datetime import datetime
+from dateutil import parser, tz
 
 import requests
 from lxml import etree
@@ -10,8 +12,15 @@ resp.raise_for_status()
 content = resp.text
 tree = etree.HTML(content)
 
-symbols = tree.xpath("//*[contains(@class, 'intraday__close')]//*[contains(@class, 'table__cell')][1]")
-
+dates = tree.xpath("//*[contains(@field, 'date')]")
+#date = datetime.strftime(dates[0].text, '%b ')
+_date = parser.parse(dates[0].text)
+_date = _date.replace(tzinfo=tz.gettz('America/New_York'))
+_date = _date.astimezone(tz.gettz('Asia/Taipei'))
+print(_date)
+'''
+price = tree.xpath("//*[contains(@field, 'Last')]")
 
 # get stock info
-pprint(symbols[0].text)
+print(symbols[0].text, dates[0].text, price[0].text)
+'''
